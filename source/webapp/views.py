952b227a402e68e4b31f29c65d0643359d84555e
uuljan <u.aitnazarova@gmail.com>
2021-02-21 13:02:30 +0600

    Completed the main logic of the game

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .check import Check, track_errors
+
+def game_view(request):
+    if request.method == 'GET':
+        return render(request, 'game.html')
+    elif request.method == 'POST':
+        try:
+            message = ''
+            numbers = list(map(int, request.POST.get('numbers').split(' ')))
+            print(numbers)
+            error = track_errors(numbers)
+            if error:
+                message += error
+            else:
+                check = Check(numbers)
+                result = check.check_values()
+                message += result
+            return render(request, 'history.html', {'message' : message})
+        except ValueError:
+            message += 'The values should be integers'
+
+
+
+
+
+
