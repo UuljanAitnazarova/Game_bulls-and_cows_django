2f8dcbde9229396508d58616d00a6bea1dbae7a9
uuljan <u.aitnazarova@gmail.com>
2021-02-21 12:58:57 +0600

    Updated urls.py for the main game page

--- source/game/urls.py
+++ source/game/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import game_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', game_view),
 ]
